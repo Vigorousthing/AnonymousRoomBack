13fbb57ddfc39f2293fdd2d90c24c0392b70952c
jhpark <pjho123456@gmail.com>
2020-06-28 18:44:38 +0900

    poll ux added

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,36 +1,110 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
+from django.core.serializers.json import DjangoJSONEncoder
+import datetime
 import json
 
 
-total_num_per_group = {}
+total_num_of_message_per_group = {}
+total_num_of_post_per_group = {}
+
+total_num_of_group_members = {}
 like_num_per_group = {}
+
+vote_state_per_group = {}
 
 
 def response_json(json_request, group_name):
-    message = json_request['message']
-    opinion = json_request['opinion']
-    tot_num = total_num_per_group[group_name]
+    tot_num = total_num_of_message_per_group[group_name]
     if json_request['type'] == "chat_message":
+        message = json_request['message']
+        opinion = json_request['opinion']
+        send_time = json_request['time']
         return {
             'type': 'chat_message',
             'message': message,
             'opinion': opinion,
+            'time': send_time,
             'id': tot_num
         }
     elif json_request['type'] == "live_like":
+        message = json_request['message']
+        opinion = json_request['opinion']
+        send_time = json_request['time']
         if json_request['id'] in like_num_per_group[group_name]:
-            like_num_per_group[group_name][json_request['id']][0] += 1
+            like_num_per_group[group_name][json_request['id']][0] += \
+                json_request['likeness']
         else:
-            like_num_per_group[group_name][json_request['id']] = [1,
+            like_num_per_group[group_name][json_request['id']] = [json_request['likeness'],
                                                                   message,
                                                                   opinion,
+                                                                  send_time,
                                                                   json_request['id']]
 
         return {
             'type': 'live_like',
+            # like num / message / opinion / send_time / id
             'like_list': sorted([like_num_per_group[group_name][key]
                           for key in like_num_per_group[group_name]],
                                 reverse=True)
+        }
+    elif json_request['type'] == "poll_post":
+        p_id = total_num_of_message_per_group[group_name]
+        now = datetime.datetime.now()
+        t = str(now)
+
+        vote_state_per_group[group_name][p_id] = {
+            'post': {
+                'id': p_id,
+                'title': json_request['title'],
+                'poll_items': json_request['poll_items'],
+                'start_time': t,
+                'end': False,
+            },
+            'vote_state': [0 for i in range(
+                len(json_request['poll_items']))],
+        }
+
+        return {
+            'type': 'poll_view',
+            'poll_info': [vote_state_per_group[group_name][each] for each
+                              in vote_state_per_group[group_name]]
+        }
+
+    elif json_request['type'] == 'poll_vote':
+
+        p_id = json_request['id']
+        idx = json_request['vote_idx']
+
+        # check whether vote is end or not
+        if vote_state_per_group[group_name][p_id]['post']['end'] is True:
+            return {
+                'type': 'poll_view',
+                'poll_info': [vote_state_per_group[group_name][each] for each
+                              in vote_state_per_group[group_name]]
+            }
+        # if vote isn't over yet, check end time is passed
+        else:
+            e_time = datetime.datetime.strptime(
+                vote_state_per_group[group_name][p_id]['post'][
+                    'start_time'].split(".")[0],
+                '%Y-%m-%d %H:%M:%S') + datetime.timedelta(minutes=1)
+            now = datetime.datetime.now()
+            # if end time is passed, make end flag True and return view
+            if e_time < now:
+                vote_state_per_group[group_name][p_id]["post"]['end'] = True
+            # else, apply vote and return view
+            else:
+                vote_state_per_group[group_name][p_id]["vote_state"][idx] += 1
+            return {
+                'type': 'poll_view',
+                'poll_info': [vote_state_per_group[group_name][each] for each
+                              in vote_state_per_group[group_name]]
+            }
+    else:
+        content = json_request['content']
+        return {
+            'type': 'multimedia',
+            'like_list': content
         }
 
 
@@ -39,10 +113,12 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
 
-        if self.room_group_name not in total_num_per_group:
-            total_num_per_group[self.room_group_name] = 0
+        if self.room_group_name not in total_num_of_message_per_group:
+            total_num_of_message_per_group[self.room_group_name] = 0
         if self.room_group_name not in like_num_per_group:
             like_num_per_group[self.room_group_name] = {}
+        if self.room_group_name not in vote_state_per_group:
+            vote_state_per_group[self.room_group_name] = {}
 
         # Join room group
         await self.channel_layer.group_add(
@@ -50,6 +126,19 @@ class ChatConsumer(AsyncWebsocketConsumer):
             self.channel_name
         )
         await self.accept()
+        
+        # send welcome packet
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+            'type': 'welcome',
+            'like_list': sorted([like_num_per_group[self.room_group_name][key]
+                          for key in like_num_per_group[self.room_group_name]],
+                                reverse=True),
+            'poll_info': [vote_state_per_group[self.room_group_name][each] for
+                          each in vote_state_per_group[self.room_group_name]]
+            }
+        )
 
     async def disconnect(self, close_code):
         # Leave room group
@@ -59,20 +148,37 @@ class ChatConsumer(AsyncWebsocketConsumer):
         )
 
     # Receive message from WebSocket
-    async def receive(self, text_data):
-        total_num_per_group[self.room_group_name] += 1
-        text_data_json = json.loads(text_data)
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            response_json(text_data_json, self.room_group_name)
-        )
+    async def receive(self, text_data=None, bytes_data=None):
+        total_num_of_message_per_group[self.room_group_name] += 1
+
+        if text_data is None:
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'multimedia',
+                    'content': bytes_data
+                }
+            )
+        else:
+            text_data_json = json.loads(text_data)
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                response_json(text_data_json, self.room_group_name)
+            )
 
     # Receive message from room group
     async def chat_message(self, event):
         await self.send(text_data=json.dumps(event))
 
     async def live_like(self, event):
-        # Send message to WebSocket
         await self.send(text_data=json.dumps(event))
 
+    async def welcome(self, event):
+        await self.send(text_data=json.dumps(event))
 
+    async def poll_view(self, event):
+        await self.send(text_data=json.dumps(event))
+
+    async def multimedia(self, event):
+        # await self.send(text_data=json.dumps(event))
+        await self.send(bytes_data=event['content'])
--- test.py
+++ test.py
@@ -1,4 +1,36 @@
-def application(env, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    return [b'Hello World']
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+#
+# url = 'http://daum.net'
+# # url = 'http://naver.com'
+# # url = 'http://twitter.com'
+# html = urlopen(url)
+# bs = BeautifulSoup(html, 'html.parser')
+#
+# # for meta in bs.head.find_all('meta'):
+# #     # print(meta.get('name'))
+# #     print(meta)
+# # image = bs.head.find('meta', {'name': 'twitter:image'}).get('content')
+# # description = bs.head.find('meta', {'name': 'description'}).get('content')
+# # print(image)
+# # print(description)
+# # title = bs.find('meta', property='og:title')['content']
+# # image = bs.find('meta', property='og:image')['content']
+# title = bs.find('title')
+# image = bs.find('image')
+#
+# print(title, image)
+
+
+class A:
+    def __init__(self, name):
+        self.name = name
+        self.a = 1
+
+
+c = A("abc")
+
+
+
+
 
